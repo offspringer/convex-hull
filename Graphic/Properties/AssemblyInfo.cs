c214b4db9ba55bbe85dba28496a5be39c5655c7a
Roger Santos <rogertdsantos@gmail.com>
2015-07-06 23:54:20 -0300

    Fixed assembly info.

--- Graphic/Properties/AssemblyInfo.cs
+++ Graphic/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Graphic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Banco Itau S.A.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Graphic")]
-[assembly: AssemblyCopyright("Copyright © Banco Itau S.A. 2009")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
